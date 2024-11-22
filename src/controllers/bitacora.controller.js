706d6bf2b7801c45bdbefab8de8618f6b11b7c16
el7jefe7hormiga <el_jefe_hormiga@hotmail.com>
2024-11-22 16:08:14 -0700

    getBitacorasTarea

--- src/controllers/bitacora.controller.js
+++ src/controllers/bitacora.controller.js
@@ -141,6 +141,7 @@ module.exports = {
   getBitacoras,
   getBitacorasCompletadas,
   getBitacorasPendientes,
+  getBitacorasTarea,
   getBitacorasTareaCompletadas,
   getBitacorasTareaPendientes,
   getHistorialJugador,
